b544b367141fbb092a2a189591db2373812b52ec
Otacilio <otaciliodearaujo@gmail.com>
2020-07-05 11:11:45 -0300

    Adicionei o suporte à calibração de cores no programa do seguidor de linha.

--- Exemplos/exemplo07SegueFaixa/EV3SegueFaixa/main.py
+++ Exemplos/exemplo07SegueFaixa/EV3SegueFaixa/main.py
@@ -13,29 +13,103 @@ import socket
 # Click "Open user guide" on the EV3 extension tab for more information.
 NAMOSTRAS = 100
 
-def calibraCor(sensorLeft, sensorRight):
-    corLeft  = []
-    corRight = []
-    while(len(corLeft)<NAMOSTRAS):
-        corLeft.append(sensorLeft.rgb())
-        corRight.append(sensorRight.rgb())
+def leCorMedia(sensorLeft, sensorRight):
+    corLeftRed      = 0
+    corLeftGreen    = 0
+    corLeftBlue     = 0
+    corRightRed     = 0
+    corRightGreen   = 0
+    corRightBlue    = 0
+    for i in range(0, NAMOSTRAS):
+        sensorEsquerdo = sensorLeft.rgb()
+        sensorDireito  = sensorRight.rgb()
+
+        corLeftRed      += sensorEsquerdo[0]
+        corLeftGreen    += sensorEsquerdo[1]
+        corLeftBlue     += sensorEsquerdo[2]
+
+        corRightRed      += sensorDireito[0]
+        corRightGreen    += sensorDireito[1]
+        corRightBlue     += sensorDireito[2]
     
-    return (corLeft, corRight)
+    corLeftRed      = corLeftRed/NAMOSTRAS
+    corLeftGreen    = corLeftGreen/NAMOSTRAS
+    corLeftBlue     = corLeftBlue/NAMOSTRAS
+
+    corRightRed     = corRightRed/NAMOSTRAS
+    corRightGreen   = corRightGreen/NAMOSTRAS
+    corRightBlue    = corRightBlue/NAMOSTRAS
+
+    return (corLeftRed, corLeftGreen, corLeftBlue, corRightRed, corRightGreen, corRightBlue)
+
+def encontraErroRGB(corRGB):
+    return (corRGB[0]-corRGB[3], corRGB[1]-corRGB[4], corRGB[2]-corRGB[5])
+
+def minimosQuadradosRGB(Y, X):
+    red     = 0
+    green   = 1
+    blue    = 2
+    n       = len(X)
+
+    sumxyr  = sumxyg = sumxyb = 0
+    sumxr   = sumxg  = sumxb  = 0
+    sumyr   = sumyg  = sumyb  = 0
+    sumx2r  = sumx2g = sumx2b = 0
+    for i in range(0, n):
+        sumxyr += X[i][red]*Y[i][red]
+        sumxr  += X[i][red]
+        sumyr  += Y[i][red]
+        sumx2r += X[i][red]**2
+
+        sumxyg += X[i][green]*Y[i][green]
+        sumxg  += X[i][green]
+        sumyg  += Y[i][green]
+        sumx2g += X[i][green]**2
+
+        sumxyb += X[i][blue]*Y[i][blue]
+        sumxb  += X[i][blue]
+        sumyb  += Y[i][blue]
+        sumx2b += X[i][blue]**2
+    
+    a1 = ((n*sumxyr - sumxr*sumyr) / (n*sumx2r - sumxr**2), (n*sumxyg - sumxg*sumyg) / (n*sumx2g - sumxg**2), (n*sumxyb - sumxb*sumyb) / (n*sumx2b - sumxb**2))
+    a0 = (sumyr/n - a1[red]*sumxr/n, sumyg/n - a1[green]*sumxg/n, sumyb/n - a1[blue]*sumxb/n)
+
+    return (a1, a0)
 
 def opcoesMenuPrincipal():
     ev3.speaker.say("Executando menu principal")
-    ev3.speaker.say("Para cima, calibração de cores")
-    ev3.speaker.say("Para direita, inicia a prova")
-    ev3.speaker.say("Para esquerda, pausa a prova")
-    ev3.speaker.say("Centro, encerra o programa")
+    botoes = ev3.buttons.pressed()
+    if(len(botoes) == 0):
+        ev3.speaker.say("Para cima, calibração de cores")
+        botoes = ev3.buttons.pressed()
+    if(len(botoes) == 0):
+        ev3.speaker.say("Para direita, inicia a prova")
+        botoes = ev3.buttons.pressed()
+    if(len(botoes) == 0):
+        ev3.speaker.say("Para esquerda, pausa a prova")
+        botoes = ev3.buttons.pressed()
+    if(len(botoes) == 0):
+        ev3.speaker.say("Centro, encerra o programa")
 
-def executaCalibracao(sensorLeft, sensorRight):
+def executaCalibracao(sensorLeft, sensorRight, contador, udp):
     ev3.speaker.say("Iniciando calibração")
-    ev3.speaker.say("Para cima, branco")
-    ev3.speaker.say("Para baixo, preto")
-    ev3.speaker.say("Para esquerda, verde")
-    ev3.speaker.say("Para direita, cinza")
-    ev3.speaker.say("Centro, retorna ao menu principal")
+    botoes = ev3.buttons.pressed()
+    if(len(botoes) == 0):
+        ev3.speaker.say("Para cima, branco")
+        botoes = ev3.buttons.pressed()
+    if(len(botoes) == 0):
+        ev3.speaker.say("Para baixo, preto")
+        botoes = ev3.buttons.pressed()
+    if(len(botoes) == 0):
+        ev3.speaker.say("Para esquerda, verde")
+        botoes = ev3.buttons.pressed()
+    if(len(botoes) == 0):
+        ev3.speaker.say("Para direita, cinza")
+        botoes = ev3.buttons.pressed()
+    if(len(botoes) == 0):
+        ev3.speaker.say("Centro, retorna ao menu principal")
+        botoes = ev3.buttons.pressed()
+    
     cronometro = StopWatch()
     cronometro.reset()
     botoes = []
@@ -49,23 +123,50 @@ def executaCalibracao(sensorLeft, sensorRight):
             cronometro.reset()
         elif([Button.UP] == botoes and cronometro.time()>=250):        
             ev3.speaker.say("Calibrando branco")
-            (brancoLeft, brancoRight) = calibraCor(sensorLeft, sensorRight)
+            branco = leCorMedia(sensorLeft, sensorRight)
+            erroBranco = encontraErroRGB(branco)
             ev3.speaker.say("Feito")
         elif([Button.DOWN] == botoes and cronometro.time()>=250):        
             ev3.speaker.say("Calibrando preto")
-            (pretoLeft, pretoRight) = calibraCor(sensorLeft, sensorRight)
+            preto = leCorMedia(sensorLeft, sensorRight)
+            erroPreto = encontraErroRGB(preto)
             ev3.speaker.say("Feito")
         elif([Button.LEFT] == botoes and cronometro.time()>=250):        
             ev3.speaker.say("Calibrando verde")
-            (verdeLeft, verdeRight) = calibraCor(sensorLeft, sensorRight)
+            verde = leCorMedia(sensorLeft, sensorRight)
+            erroVerde = encontraErroRGB(verde)
             ev3.speaker.say("Feito")
         elif([Button.RIGHT] == botoes and cronometro.time()>=250):        
             ev3.speaker.say("Calibrando cinza")
-            (cinzaLeft, cinzaRight) = calibraCor(sensorLeft, sensorRight)
+            cinza = leCorMedia(sensorLeft, sensorRight)
+            erroCinza = encontraErroRGB(cinza)
             ev3.speaker.say("Feito")
         elif([Button.CENTER] == botoes and cronometro.time()>=250):
             break
+        
+        left = corLeft.rgb()
+        right= corRight.rgb()
+
+        lr = "{0:.2f}".format(left[0])
+        lg = "{0:.2f}".format(left[1])
+        lb = "{0:.2f}".format(left[2])
+
+        rr = "{0:.2f}".format(right[0])
+        rg = "{0:.2f}".format(right[1])
+        rb = "{0:.2f}".format(right[2])
+
+        contador += 1
+
+        msg = str(contador)+" "+lr+" "+lg+" "+lb+" "+rr+" "+rg+" "+rb
+
+        udp.sendto (msg, dest)
+        
         botoesAnt = botoes
+        
+    X = [(branco[3],     branco[4],     branco[5]),     (preto[3],     preto[4],     preto[5]),     (verde[3],     verde[4],     verde[5]),     (cinza[3],     cinza[4],     cinza[5])]
+    Y = [(erroBranco[0], erroBranco[1], erroBranco[2]), (erroPreto[0], erroPreto[1], erroPreto[2]), (erroVerde[0], erroVerde[1], erroVerde[2]), (erroCinza[0], erroCinza[1], erroCinza[2])]
+    (a1, a0) = minimosQuadradosRGB(Y, X)
+    return (a1, a0, contador)
 
 # Create your objects here.
 ev3 = EV3Brick()
@@ -73,6 +174,11 @@ ev3 = EV3Brick()
 # Write your program here.
 ev3.speaker.set_speech_options('pt-br','m3')
 ev3.speaker.beep()
+
+HOST = '192.168.0.3'    # Endereco IP do Servidor
+PORT = 2508             # Porta que o Servidor esta
+udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+dest = (HOST, PORT)
 
 # Motores
 motorLeft = Motor(Port.B)
@@ -101,6 +207,9 @@ ev3.speaker.say("Iniciando programa de controle")
 botoes = []
 botoesAnt = botoes
 opcoesMenuPrincipal()
+a1 = (0, 0, 0)
+a0 = (0, 0, 0)
+contador = 0
 while(True):
     # Vamos implementar o menu principal
     botoes = ev3.buttons.pressed()
@@ -109,10 +218,28 @@ while(True):
     elif(botoes != botoesAnt):
         cronometro.reset()
     elif([Button.UP] == botoes and cronometro.time()>=250):        
-        executaCalibracao(corLeft, corRight)        
+        (a1, a0, contador) = executaCalibracao(corLeft, corRight, contador, udp)        
         opcoesMenuPrincipal()
     elif([Button.CENTER] == botoes and cronometro.time()>=250):
         break
+
+    left = corLeft.rgb()
+    right= corRight.rgb()
+
+    lr = "{0:.2f}".format(left[0])
+    lg = "{0:.2f}".format(left[1])
+    lb = "{0:.2f}".format(left[2])
+
+    rr = "{0:.2f}".format(right[0]+a1[0]*right[0] + a0[0])
+    rg = "{0:.2f}".format(right[1]+a1[1]*right[1] + a0[1])
+    rb = "{0:.2f}".format(right[2]+a1[2]*right[2] + a0[2])
+
+    contador += 1
+
+    msg = str(contador)+" "+lr+" "+lg+" "+lb+" "+rr+" "+rg+" "+rb
+    #print("Tempo formatação ", cronometro.time())
+
+    udp.sendto (msg, dest)
 
     botoesAnt = botoes
 
